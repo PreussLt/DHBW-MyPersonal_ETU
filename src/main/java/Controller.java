65fb40e22a5a2ac7b365b897c5577a7feb17ca5c
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-09-10 23:34:46 +0200

    Update Controller.java

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,12 +1,14 @@
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 
 public class Controller {
   public static sql_statment sql = new sql_statment();
+  public static Passwort_verwaltung pwv = new Passwort_verwaltung();
 
-  public static void main(String[] args) {
-    Connection ETU = sql_connect.etu_connect();
-   sql.select_arr("uk_userklassen","*","",ETU);
-   System.out.println(sql.get_numColums("uk_userklassen",ETU));
+  public static void main(String[] args) throws NoSuchAlgorithmException {
 
+    String salt = pwv.get_Salt();
+    System.out.println(pwv.prüfePasswort("Test1234", pwv.get_hash("Test1234", salt), salt));
+
+    }
   }
-}
