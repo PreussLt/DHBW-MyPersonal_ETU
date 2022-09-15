1f2a3f85bafebf8a94723dba39635b603d85216c
Noche9001 <64547700+Noche9001@users.noreply.github.com>
2022-09-15 10:17:33 +0200

    -Test zum Zusammenspiel von Front- und Backend -Übersetzung möglich

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
+import {TimeEntryService} from "./timeEntry/service/time-entry.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+// import ngx-translate and the http loader
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient} from '@angular/common/http';
+import { HomeComponent } from './home/component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [TimeEntryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
---
+++ src/app/timeEntry/model/time-entry.spec.ts
@@ -0,0 +1,7 @@
+import { TimeEntry } from './time-entry';
+
+describe('TimeEntry', () => {
+  it('should create an instance', () => {
+    expect(new TimeEntry()).toBeTruthy();
+  });
+});
