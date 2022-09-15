04be480f1e42fcb02400bdbedda807d8675eb6fa
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-09-15 08:28:00 +0200

    Update Buchungsdaten
    Funktion berechne Arbeitszeit hinzugefügt

--- src/main/java/db/GetBuchungsdaten.java
+++ src/main/java/db/GetBuchungsdaten.java
@@ -18,19 +18,25 @@ public class GetBuchungsdaten {
     return -1;
   }// get ArbeitszeitHeute
 
-  private int berechneArbeitszeit(String mid,String Tag){
+  private double berechneArbeitszeit(String mid,String Tag){
     Connection con = sql_conn.extern_connect();
     if (!nzv.existiertNutzer(mid)) return -1;
-    String[][] arbzeit = sql.select_arr(cnf.mb_zeiteintrag+","+cnf.mb_buchung,"*","B_ID = BZ_B_ID AND B_M_ID=\'"+mid+"\' ORDER BY BZ_Zeiteintrag DESC ",con);
+    String[][] arbzeit = sql.select_arr(cnf.mb_zeiteintrag+","+cnf.mb_buchung,"*","B_ID = BZ_B_ID AND B_M_ID=\'"+mid+"\' ORDER BY BZ_Zeiteintrag ASC ",con);
     if((arbzeit.length%2)==1){
       System.err.println("!ERROR! Es fehlt ein Zeiteintrag");
       return -1;
     }// END IF, Es ist eine Ungerade Zahl
-    int Arbeitszeit = 0;
+    double Arbeitszeit = 0;
     for (int i = 0; i>= (arbzeit.length/2);i=i+2){
-      //int x = arbzeit[i][5] - arbzeit[i+1][5];
+      try {
+        Arbeitszeit = getDifTime(arbzeit[i][5],arbzeit[i+1][0]);
+      } catch (Exception e){
+        System.err.println("!ERROR! Es enstand ein Fehler beim berechnen entstanden");
+        return -1;
+      }
+
     }
-    return 1;
+    return Arbeitszeit;
   }
 
   private String getHeute(){
@@ -41,14 +47,16 @@ public class GetBuchungsdaten {
     return d_heute;
   }// Get Heuter
 
-  public int getDifTime(String eintrag1, String eintrag2) throws ParseException {
+  public double getDifTime(String eintrag1, String eintrag2) throws ParseException {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     LocalDateTime dateTime1= LocalDateTime.parse(eintrag1, formatter);
     LocalDateTime dateTime2= LocalDateTime.parse(eintrag2, formatter);
 
     long diffInMinutes = java.time.Duration.between(dateTime1, dateTime2).toMinutes();
-    System.out.println((diffInMinutes/60)+" "+(diffInMinutes%60));
-    return (int)diffInMinutes;
+    int stunden = (int)diffInMinutes/60;
+    double ausgabe = (diffInMinutes-stunden*60)*100/60;
+    ausgabe = ausgabe/100 + (double) stunden ;
+    return ausgabe;
   }//
 }
