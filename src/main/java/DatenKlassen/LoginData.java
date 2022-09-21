2d49db325e8000283b26339acb0e1de7a8c3dd14
Noche9001 <64547700+Noche9001@users.noreply.github.com>
2022-09-21 13:37:00 +0200

    feature/doku

--- src/main/java/DatenKlassen/LoginData.java
+++ src/main/java/DatenKlassen/LoginData.java
@@ -2,6 +2,11 @@ package DatenKlassen;
 
 import lombok.Data;
 
+/**
+ * Klasse fuer Anmeldeinformationen
+ * @author Noah Dambacher
+ * @version 1.0
+ */
 @Data
 public class LoginData {
   private String personalnummer;
