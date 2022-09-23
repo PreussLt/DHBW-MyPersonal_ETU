5c19f809f00de4ebf28b20f329016e5830aaafb5
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-09-23 15:22:23 +0200

    Hotfix

--- src/main/java/DatenKlassen/Arbeitstag.java
+++ src/main/java/DatenKlassen/Arbeitstag.java
@@ -30,6 +30,7 @@ public class Arbeitstag {
 
   public Arbeitstag(String tag, double arbeitszeit, String[][] zeitstempel, String ersterStempel, String letzterStempel, String mid) {
 
+
     this.tag = tag;
     this.arbeitszeit = arbeitszeit;
     this.zeitstempel = zeitstempel;
@@ -78,7 +79,67 @@ public class Arbeitstag {
     return false;
   }
 
-  private double Sollarbetiszeit() {
+  private double Sollarbetiszeit(){
     return 7.6; // TODO: 19.09.2022 Hier die Datenbank verbindung aufbauen und den Berechneten Wert ausgeben
+  }
+
+
+
+
+  // Getter und Setter @AutoGenerarated
+  public String getTag() {
+    return tag;
+  }
+
+  public void setTag(String tag) {
+    this.tag = tag;
+  }
+
+  public double getArbeitszeit() {
+    return arbeitszeit;
+  }
+
+  public void setArbeitszeit(double arbeitszeit) {
+    this.arbeitszeit = arbeitszeit;
+  }
+
+  public String[][] getZeistempel() {
+    return this.zeitstempel;
+  }
+
+  public void setZeistempel(String[][] zeistempel) {
+    this.zeitstempel = zeistempel;
+  }
+
+  public String getErsterStempel() {
+    return ersterStempel;
+  }
+
+  public void setErsterStempel(String ersterStempel) {
+    this.ersterStempel = ersterStempel;
+  }
+
+  public String getLetzterStempel() {
+    return letzterStempel;
+  }
+
+  public void setLetzterStempel(String letzterStempel) {
+    this.letzterStempel = letzterStempel;
+  }
+
+  public boolean isFeiertag() {
+    return feiertag;
+  }
+
+  public void setFeiertag(boolean feiertag) {
+    this.feiertag = feiertag;
+  }
+
+  public boolean isGleitzeittag() {
+    return gleitzeittag;
+  }
+
+  public void setGleitzeittag(boolean gleitzeittag) {
+    this.gleitzeittag = gleitzeittag;
   }
 }
--- src/main/java/DatenKlassen/ArbeitstagListe.java
+++ src/main/java/DatenKlassen/ArbeitstagListe.java
@@ -33,19 +33,31 @@ public class ArbeitstagListe {
 
   }// Constructor
 
-  public int getGleitzeit(){
-    int gleitzeit = 0;
-    for (int i=0; i<arbeitstage.length;i++){
-      gleitzeit += arbeitstage[i].getArbeitszeit();
+
+  private int getAnzahlArbeitstage() {
+    try {
+      if (sql_statment.select_arr(cnf.mb_buchung, "count(*)", "WHERE B_M_ID=\'" + mid + "\'", con)[0][0].equals(0)) return -1;
+      int tage = Integer.parseInt(sql_statment.select_arr(cnf.mb_buchung, "count(*)", "WHERE B_M_ID=\'" + mid + "\'", con)[0][0]);
+      tage += Integer.parseInt(sql_statment.select_arr(cnf.gleitzeittage, "count(*)", "WHERE MG_M_ID=\'" + mid + "\'", con)[0][0]);
+      return tage;
+    } catch (Exception e) {
+      System.err.println("!ERROR! Fehler in der Anzahl Arbeitstag: " + e);
+      return -1;
     }
-    return gleitzeit;
   }
 
+    private int getAnzahlArbeitstage(String eDatum, String aDatum){
+    try {
+      if (sql_statment.select_arr(cnf.mb_buchung,"count(*)","WHERE B_M_ID=\'"+mid+"\' AND B_TAG >\'"+aDatum+"\' AND B_TAG <=\'"+eDatum+"\'",con)[0][0].equals(0)) return -1;
+      int tage = Integer.parseInt(sql_statment.select_arr(cnf.mb_buchung,"count(*)","WHERE B_M_ID=\'"+mid+"\' AND B_TAG >\'"+aDatum+"\' AND B_TAG <=\'"+eDatum+"\'",con)[0][0]);
+      tage += Integer.parseInt(sql_statment.select_arr(cnf.gleitzeittage,"count(*)","WHERE MG_M_ID=\'"+mid+"\' AND MG_TAG >\'"+aDatum+"\' AND MG_TAG <=\'"+eDatum+"\'",con)[0][0]);
+      return tage;
+    } catch (Exception e){
+      System.err.println("!ERROR! Fehler in der Anzahl Arbeitstag: " + e);
+      return -1;
+    }
+    // get AnzahlArbeitstage
 
-
-  private int getAnzahlArbeitstage(String eDatum, String aDatum){
-    if (sql_statment.select_arr(cnf.mb_buchung,"count(*)","WHERE B_M_ID=\'"+mid+"\' AND B_TAG >\'"+aDatum+"\' AND B_TAG <=\'"+eDatum+"\'",con)[0][0].equals(0)) return -1;
-    return Integer.parseInt(sql_statment.select_arr(cnf.mb_buchung,"count(*)","WHERE B_M_ID=\'"+mid+"\'",con)[0][0]);
   }// get AnzahlArbeitstage
 
   private boolean getArbeitstage(int laengeListe){
