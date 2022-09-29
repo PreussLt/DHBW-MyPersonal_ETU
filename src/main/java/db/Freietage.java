832cec066f87357296b4a7754fedc8695427f09c
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-09-29 16:30:11 +0200

    Update Freietage.java
    Hinzufügen Funktion:
    setGleitzeittag
    setUrlaubstag

--- src/main/java/db/Freietage.java
+++ src/main/java/db/Freietage.java
@@ -2,7 +2,15 @@ package db;
 
 import DatenKlassen.ArbeitstagListe;
 
+import java.sql.Connection;
+
 public class Freietage {
+  // Andere Klassen
+  private static sql_statment sql = new sql_statment();
+  private static Connection con = sql_connect.intern_connect();
+  private static Einstellungen cnf = new Einstellungen();
+
+
   public double getGleitzeitstand(ArbeitstagListe ab){
     double ausgabe = 0;
     for (int i=0; i<ab.arbeitstage.length;i++){
@@ -10,4 +18,25 @@ public class Freietage {
     }
     return ausgabe;
   }// GetGleitzeitstand
+
+  public boolean setGleitzeittag(String tag,String mid){
+    try {
+      String[] Daten = {mid,"0",tag};
+      return sql.insert(cnf.gleitzeittage,Daten,con);
+    }catch (Exception e){
+      System.err.println("!ERROR! Fehler in setGleitzeittag: "+e);
+      return false;
+    }
+  }// setGleitzeitata
+
+  public boolean setUrlaubstag(String tag,String mid){
+    try {
+      String[] Daten = {mid,"1",tag};
+      return sql.insert(cnf.gleitzeittage,Daten,con);
+    }catch (Exception e){
+      System.err.println("!ERROR! Fehler in setUrlaubstag: "+e);
+      return false;
+    }
+  }// setGleitzeitata
+
 }// Class
