f960a5868fdccf05a8729442b950a5b25fa38da4
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2022-10-28 14:34:30 +0200

    Update SSO.java

--- src/main/java/db/SSO.java
+++ src/main/java/db/SSO.java
@@ -34,37 +34,43 @@ public class SSO {
   }
 
     public String identifizieren() {  // Identifiziert den Nutzer via SSO
-      // initialize a security context on the client
-      IWindowsSecurityContext clientContext = WindowsSecurityContextImpl.getCurrent( "Negotiate", "localhost" );
+      try {
+        // initialize a security context on the client
+        IWindowsSecurityContext clientContext = WindowsSecurityContextImpl.getCurrent( "Negotiate", "localhost" );
 
-      // create an auth provider and a security context for the client
-      // on the server
-      WindowsAuthProviderImpl provider = new WindowsAuthProviderImpl();
-      IWindowsSecurityContext serverContext = null;
+        // create an auth provider and a security context for the client
+        // on the server
+        WindowsAuthProviderImpl provider = new WindowsAuthProviderImpl();
+        IWindowsSecurityContext serverContext = null;
 
-      // now you would send the byte[] token to the server and the server will
-      // response with another byte[] token, which the client needs to answer again
-      do {
+        // now you would send the byte[] token to the server and the server will
+        // response with another byte[] token, which the client needs to answer again
+        do {
 
-        // Step 2: If you have already build an initial security context for the client
-        // on the server, send a token back to the client, which the client needs to
-        // accept and send back to the server again (a handshake)
-        if (serverContext != null) {
-          byte[] tokenForTheClientOnTheServer = serverContext.getToken();
-          SecBufferDesc continueToken = new SecBufferDesc(Sspi.SECBUFFER_TOKEN, tokenForTheClientOnTheServer);
-          clientContext.initialize(clientContext.getHandle(), continueToken, "localhost");
-        }
+          // Step 2: If you have already build an initial security context for the client
+          // on the server, send a token back to the client, which the client needs to
+          // accept and send back to the server again (a handshake)
+          if (serverContext != null) {
+            byte[] tokenForTheClientOnTheServer = serverContext.getToken();
+            SecBufferDesc continueToken = new SecBufferDesc(Sspi.SECBUFFER_TOKEN, tokenForTheClientOnTheServer);
+            clientContext.initialize(clientContext.getHandle(), continueToken, "localhost");
+          }
 
-        // Step 1: accept the token on the server and build a security context
-        // representing the client on the server
-        byte[] tokenForTheServerOnTheClient = clientContext.getToken();
-        serverContext = provider.acceptSecurityToken("server-connection", tokenForTheServerOnTheClient, "Negotiate");
+          // Step 1: accept the token on the server and build a security context
+          // representing the client on the server
+          byte[] tokenForTheServerOnTheClient = clientContext.getToken();
+          serverContext = provider.acceptSecurityToken("server-connection", tokenForTheServerOnTheClient, "Negotiate");
 
-      } while (clientContext.isContinue());
+        } while (clientContext.isContinue());
 
-      // at the end of this handshake, we know on the server side who the
-      // client is, only by exchanging byte[] arrays
-     return serverContext.getIdentity().getFqn();
+        // at the end of this handshake, we know on the server side who the
+        // client is, only by exchanging byte[] arrays
+        return serverContext.getIdentity().getFqn();
+      }catch (Exception e){
+        System.err.println("Fehler in indentifizieren: "+e);
+        return null;
+      }
+
     }
   }
 
